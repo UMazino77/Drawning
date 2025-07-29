bf3dc0ca79e14b8b3bf07bb66bb9157f773587da
matmani <mohamedatm2003@gmail.com>
2025-07-29 16:50:07 +0100

    drawing  ===>  still needs a lot of refining

--- src/geometrical_shapes.rs
+++ src/geometrical_shapes.rs
@@ -1,11 +1,16 @@
 use rand::random;
 use raster::Color;
-use raster::Image;
 
 pub trait Drawable {
-        fn draw(im : &mut Image) ;
+    fn draw(&self, im: &mut dyn Displayable);
 
-        fn color()->Color ;
+    fn color() -> Color {
+        let r = random::<u8>();
+        let g = random::<u8>();
+        let b = random::<u8>();
+
+        Color::rgb(r, g, b)
+    }
 }
 
 pub trait Displayable {
@@ -14,17 +19,17 @@ pub trait Displayable {
 
 #[derive(Debug, Clone)]
 pub struct Point(i32, i32);
-pub struct Line(pub Point,pub Point);
-pub struct Triangle(pub Point,pub Point,pub Point);
-pub struct Rectangle(pub Point,pub Point);
-pub struct Circle(pub Point, i32);
+pub struct Line(pub Point, pub Point);
+pub struct Triangle(pub Point, pub Point, pub Point);
+// pub struct Rectangle(pub Point,pub Point);
+// pub struct Circle(pub Point, i32);
 
 impl Point {
     pub fn new(x: i32, y: i32) -> Self {
         Point(x, y)
     }
 
-    pub fn random(width : i32, height:i32) -> Self {
+    pub fn random(width: i32, height: i32) -> Self {
         let a = random::<i32>() % width;
         let b = random::<i32>() % height;
 
@@ -32,99 +37,175 @@ impl Point {
     }
 }
 
-impl color for Point {
-        fn color()->Color {
-        let r = random::<u8>();
-        let g = random::<u8>();
-        let b = random::<u8>();
-
-        Color::rgb(r,g,b)
+impl Drawable for Point {
+    fn draw(&self, im: &mut dyn Displayable) {
+        im.display(self.0, self.1, Point::color());
     }
 }
 
-impl Triangle {
-    pub fn new(x: Point , y:Point, z : Point) -> Self {
-        Triangle(x,y,z)
-    }
-}
+// impl Triangle {
+//     pub fn new(x: Point , y:Point, z : Point) -> Self {
+//         Triangle(x,y,z)
+//     }
+// }
 
-impl color for Triangle {
-        fn color()->Color {
-        let r = random::<u8>();
-        let g = random::<u8>();
-        let b = random::<u8>();
-
-        Color::rgb(r,g,b)
-    }
-}
+// impl Drawable for Triangle {
+//     fn draw(&self, im : &mut dyn Displayable) {
+//         im.display(x, y, color);
+//     }
+// }
 
 impl Line {
     pub fn new(x: Point, y: Point) -> Self {
-        Line(x,y)
+        Line(x, y)
     }
 
-    pub fn random(width : i32, height: i32) ->Self{
-        let a = random::<i32>() % width;
-        let b = random::<i32>() % height;
+    pub fn random(width: i32, height: i32) -> Self {
+        let a = random::<i32>().rem_euclid(width);
+        let b = random::<i32>().rem_euclid(height);
 
-        Line::new(a, b)
-    }
-}
+        let p1 = Point::new(a, b);
 
-impl color for Line {
-        fn color()->Color {
-        let r = random::<u8>();
-        let g = random::<u8>();
-        let b = random::<u8>();
+        let c = random::<i32>().rem_euclid(width);
+        let d = random::<i32>().rem_euclid(height);
 
-        Color::rgb(r,g,b)
+        let p2 = Point::new(c, d);
+
+        Line::new(p1, p2)
     }
 }
 
-impl Rectangle {
-    pub fn new(x : Point , y : Point) -> Self {
-        Rectangle(x, y)
-    }
-    
+impl Drawable for Line {
+   fn draw(&self, im : &mut dyn Displayable) {
+    let a = abs(self.0.0-self.1.0);
+    let b = abs(self.0.1-self.1.1);
+    let mut dis : f64 = max(a as f64, b as f64) ;
+
+    let col = Line::color();
+
+    let cc  = a as f64 / dis;
+    let dd = b as f64 / dis;
+    dis = dis.sqrt();
+    let k = dis as i32;
+        for i in 0..=k {
+            // let m: i32 = (sel4096f.1.1-self.0.1) / (self.1.0-self.0.0) ;
+            // println!("{} ===> {}",self.0.0+i*m,self.0.1+i*m);
+            // let ccc = 
+            // cc += cc;
+            // dd += dd;
+            im.display(self.0.0+(i as f64 *cc) as i32,self.0.1+(i as f64 *dd) as i32 , col.clone());
+        }
+
+   }
 }
 
-impl color for Rectangle {
-        fn color()->Color {
-        let r = random::<u8>();
-        let g = random::<u8>();
-        let b = random::<u8>();
+// impl Drawable for Line {
+//     fn draw(&self, im: &mut dyn Displayable) {
+//         // let a = self.0.0-self.1.0;
+//         // let b = self.0.1-self.1.1;
+//         // let mut dis : f64 = (a*a+b*b) as f64;
+//         // dis = dis.sqrt();
+//         // let k = dis as i32;
+//         let n: f64 = (self.1.1 - self.0.1) as f64;
+//         let p :f64 = (self.1.0 - self.0.0) as f64;
+//         let m = n/p;
+//         let mm = m as i32;
 
-        Color::rgb(r,g,b)
+//         println!("le point 1 : x = {} , y = {}\n\nle point 2 : x = {} , y = {}", self.0.0, self.0.1,self.1.0,self.1.1);
+
+//         let col = Line::color();
+
+//         let mut c_pos = 1;
+//         let mut y_pos = 1;
+//         if self.1.0-self.0.0 < 0{
+//             c_pos = -1
+//         }
+
+//         if self.1.1-self.0.1 < 0{
+//             y_pos = -1;
+//         }
+
+
+//         for y in 0..=abs(self.1.1 - self.0.1) {
+//             for x in 0..=abs(self.1.0 - self.0.0) {
+                
+               
+//                 // println!("{} ===> {}", self.0.0 + i * m, self.0.1 + i * m);
+//                 let yy = y as f64;
+//                 let xx = x as f64;
+//                 if yy >= m*xx-2.0 && yy <= m*xx+2.0 {
+//                     im.display((self.0.0 + c_pos*x), self.0.1 + y_pos*y, col.clone());
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// impl Rectangle {
+//     pub fn new(x : Point , y : Point) -> Self {
+//         Rectangle(x, y)
+//     }
+
+// }
+
+// impl color for Rectangle {
+//         fn color()->Color {
+//         let r = random::<u8>();
+//         let g = random::<u8>();
+//         let b = random::<u8>();
+
+//         Color::rgb(r,g,b)
+//     }
+// }
+
+// impl Circle {
+//     pub fn new(c : Point , r : i32) -> Self {
+//         Circle(c, r)
+//     }
+
+//     pub fn random(width:i32, height : i32) -> Self{
+//         let a = random::<i32>() % width;
+//         let b = random::<i32>() % height;
+//         let c = random::<i32>() % (min(height,width)/2);
+
+//         Circle::new(Point::new(a, b),c)
+//     }
+// }
+
+// impl color for Circle {
+//         fn color()->Color {
+//         let r = random::<u8>();
+//         let g = random::<u8>();
+//         let b = random::<u8>();
+
+//         Color::rgb(r,g,b)
+//     }
+// }
+
+pub fn min(a: i32, b: i32) -> i32 {
+    if a < b {
+        return a;
     }
+    b
 }
 
-impl Circle {
-    pub fn new(c : Point , r : i32) -> Self {
-        Circle(c, r)
+pub fn abs(a: i32) -> i32 {
+    if a < 0 {
+        return -1 * a;
     }
-
-    pub fn random(width:i32, height : i32) -> Self{
-        let a = random::<i32>() % width;
-        let b = random::<i32>() % height;
-        let c = random::<i32>() % (min(height,width)/2);
-
-
-        Circle::new(Point::new(a, b),c) 
-    }
+    a
 }
 
-impl color for Circle {
-        fn color()->Color {
-        let r = random::<u8>();
-        let g = random::<u8>();
-        let b = random::<u8>();
 
-        Color::rgb(r,g,b)
+pub fn aabs(a: f64) -> f64 {
+    if a < 0.0 {
+        return -1.0 * a;
     }
+    a
 }
 
-pub fn min(a : i32, b: i32)->i32 {
-    if a<b {
+pub fn max(a: f64, b: f64) -> f64 {
+    if a > b {
         return a;
     }
     b
